26a6a895c242f45b89dbfa9b8d15e8fbe4b507c4
Vytautas Galaunia <vytautas.galaunia@gmail.com>
2017-08-24 15:49:37 +0300

    Add couple comments to VLTGPS class

--- VelocitySDK/VLTGPS.h
+++ VelocitySDK/VLTGPS.h
@@ -14,10 +14,23 @@ extern NSString * _Nonnull const VLTGPSLocationsKey;
 
 @interface VLTGPS : NSObject
 
+
+/**
+ * @brief A point for GPS intergration, you should invoke it from your delegate of CLLocationManger
+ * @param manager Instance of CLLoationManager
+ * @param locations List of locations received by CLLocationManager delegate
+ **/
 + (void)locationManager:(nonnull CLLocationManager *)manager
      didUpdateLocations:(nonnull NSArray<CLLocation *> *)locations;
 
+/**
+ * @brief Returns current state of GPS data collection
+ */
 + (BOOL)isEnabled;
+
+/**
+ * @brief Enables GPS data collection in SDK
+ */
 + (void)setEnabled:(BOOL)enabled;
 
 @end
